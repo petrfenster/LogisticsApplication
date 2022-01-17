4e70a9914144e483256113c853cec9fbec7e8048
Petr Fenster <77027099+petrfenster@users.noreply.github.com>
2022-01-17 15:30:08 -0800

    added edit functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,10 +18,62 @@ class Product(db.Model):
         return '<Product %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        product_name = request.form['name']
+        product_description = request.form['description']
+        product_type = request.form['type']
+        product_price = request.form['price']
+        product_quantity = request.form['quantity']
 
+        new_product = Product(name=product_name,
+                              description=product_description,
+                              type=product_type,
+                              price=product_price,
+                              quantity=product_quantity)
+
+        try:
+            db.session.add(new_product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Sorry, there was an issue adding your product'
+
+    else:
+        products = Product.query.all()
+        return render_template('index.html', products=products)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    product_to_delete = Product.query.get_or_404(id)
+
+    try:
+        db.session.delete(product_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Sorry, there was a problem deleting the product'
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    product_to_edit = Product.query.get_or_404(id)
+
+    if request.method == 'POST':
+        product_to_edit.name = request.form['name']
+        product_to_edit.description = request.form['description']
+        product_to_edit.type = request.form['type']
+        product_to_edit.price = request.form['price']
+        product_to_edit.quantity = request.form['quantity']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Sorry, there was an issue editing your product's information"
+
+    else:
+        return render_template('edit.html', product=product_to_edit)
 
 if __name__ == "__main__":
     app.run(debug=True)
