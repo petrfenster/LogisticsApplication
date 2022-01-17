058041e19776cd354f32e38691c30fa12b18d3e0
Petr Fenster <77027099+petrfenster@users.noreply.github.com>
2022-01-17 11:07:40 -0800

    added db.Model

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.id
 
 
 @app.route('/')
@@ -10,4 +25,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
